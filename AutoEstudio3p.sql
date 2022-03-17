e987d4818b96301ad9b67bec4184dd8cd47fb1dd
Sebastian-Cepeda <89321404+Sebasian-Cepeda@users.noreply.github.com>
2022-03-17 18:09:54 -0500

    Update AutoEstudio3p.sql

--- AutoEstudio3p.sql
+++ AutoEstudio3p.sql
@@ -75,13 +75,13 @@ ALTER TABLE band ADD CONSTRAINT FK_band_musician
 ALTER TABLE composer ADD CONSTRAINT FK_composer_musician
     FOREIGN KEY(comp_is) REFERENCES musician(m_no);
 
-ALTER TABLE composition ADD CONSTRAINT FK_place_composition
+ALTER TABLE composition ADD CONSTRAINT FK_composition_place
     FOREIGN KEY(c_in) REFERENCES place(place_no);
 
-ALTER TABLE band ADD CONSTRAINT FK_place_band
+ALTER TABLE band ADD CONSTRAINT FK_band_place
     FOREIGN KEY(band_home) REFERENCES place(place_no);
     
-ALTER TABLE plays_in ADD CONSTRAINT FK_band_plays_in
+ALTER TABLE plays_in ADD CONSTRAINT FK_plays_in_band
     FOREIGN KEY(band_id) REFERENCES band(band_no);
     
 ALTER TABLE has_composed ADD CONSTRAINT FK_has_composed_composer
@@ -128,14 +128,66 @@ ALTER TABLE band ADD CONSTRAINT CK_BAND_BAND_NAME
     CHECK(REGEXP_LIKE(band_name, '^[[:alpha:]]{1,20}'));
 
 --- Acciones
+ALTER TABLE musician DROP CONSTRAINT FK_musician_place;
+ALTER TABLE musician DROP CONSTRAINT FK2_musician_place;
+ALTER TABLE performer DROP CONSTRAINT FK_performer_musician;
 ALTER TABLE band DROP CONSTRAINT FK_band_musician;
-ALTER TABLE band DROP CONSTRAINT FK_place_band;
+ALTER TABLE composer DROP CONSTRAINT FK_composer_musician;
+ALTER TABLE composition DROP CONSTRAINT FK_composition_place;
+ALTER TABLE band DROP CONSTRAINT FK_band_place;
+ALTER TABLE plays_in DROP CONSTRAINT FK_plays_in_band;
+ALTER TABLE has_composed DROP CONSTRAINT FK_has_composed_composer;
+ALTER TABLE plays_in DROP CONSTRAINT FK_plays_in_performer;
+ALTER TABLE has_composed DROP CONSTRAINT FK_has_composed_composition;
+ALTER TABLE concert DROP CONSTRAINT FK_concert_place;
+ALTER TABLE concert DROP CONSTRAINT FK_concert_musician;
+ALTER TABLE performances DROP CONSTRAINT FK_performances_musician;
+ALTER TABLE performances DROP CONSTRAINT FK_Performances_concert;
 
-ALTER TABLE band ADD CONSTRAINT FK_place_musician
+ALTER TABLE musician ADD CONSTRAINT FK_musician_place
+    FOREIGN KEY(born_in) REFERENCES place(place_no) ON DELETE CASCADE;
+
+ALTER TABLE musician ADD CONSTRAINT FK2_musician_place
+    FOREIGN KEY(living_in) REFERENCES place(place_no) ON DELETE CASCADE;
+
+ALTER TABLE performer ADD CONSTRAINT FK_performer_musician
+    FOREIGN KEY(perf_is) REFERENCES musician(m_no) ON DELETE CASCADE;
+
+ALTER TABLE band ADD CONSTRAINT FK_band_musician
     FOREIGN KEY(band_contact) REFERENCES musician(m_no) ON DELETE CASCADE;
 
-ALTER TABLE band ADD CONSTRAINT FK_place_band
-   FOREIGN KEY(band_home) REFERENCES place(place_no) ON DELETE CASCADE;
+ALTER TABLE composer ADD CONSTRAINT FK_composer_musician
+    FOREIGN KEY(comp_is) REFERENCES musician(m_no) ON DELETE CASCADE;
+
+ALTER TABLE composition ADD CONSTRAINT FK_composition_place
+    FOREIGN KEY(c_in) REFERENCES place(place_no) ON DELETE CASCADE;
+
+ALTER TABLE band ADD CONSTRAINT FK_band_place
+    FOREIGN KEY(band_home) REFERENCES place(place_no) ON DELETE CASCADE;
+    
+ALTER TABLE plays_in ADD CONSTRAINT FK_plays_in_band
+    FOREIGN KEY(band_id) REFERENCES band(band_no) ON DELETE CASCADE;
+    
+ALTER TABLE has_composed ADD CONSTRAINT FK_has_composed_composer
+    FOREIGN KEY(cmpr_no) REFERENCES composer(comp_no) ON DELETE CASCADE;
+
+ALTER TABLE plays_in ADD CONSTRAINT FK_plays_in_performer
+    FOREIGN KEY(player) REFERENCES performer(perf_no) ON DELETE CASCADE;
+
+ALTER TABLE has_composed ADD CONSTRAINT FK_has_composed_composition
+    FOREIGN KEY(cmpn_no) REFERENCES composition(c_no) ON DELETE CASCADE;
+
+ALTER TABLE concert ADD CONSTRAINT FK_concert_place
+    FOREIGN KEY(concert_in) REFERENCES place(place_no) ON DELETE CASCADE;
+
+ALTER TABLE concert ADD CONSTRAINT FK_concert_musician
+    FOREIGN KEY(concert_orgniser) REFERENCES musician(m_no) ON DELETE CASCADE;
+
+ALTER TABLE Performances ADD CONSTRAINT FK_performances_musician
+    FOREIGN KEY(conducted_by) REFERENCES musician(m_no) ON DELETE CASCADE;
+
+ALTER TABLE Performances ADD CONSTRAINT FK_Performances_concert
+    FOREIGN KEY(performed_in) REFERENCES concert(concert_no) ON DELETE CASCADE;
 
 
 --Poblar la base de datos con los datos iniciales (PoblarOK) Automaticen la generación de las instrucciones INSERT. Dejen en el archivo las consultas correspondientes en comentarios.
